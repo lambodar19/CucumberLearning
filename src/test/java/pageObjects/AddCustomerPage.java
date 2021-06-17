3682f900c648adf938b8e984a23a2f883d8706fc
lambodar19 <padhan.lambodar19@gmail.com>
2021-06-17 18:18:21 +0530

    End to ENd flow for Add Customer

--- src/test/java/pageObjects/AddCustomerPage.java
+++ src/test/java/pageObjects/AddCustomerPage.java
@@ -11,44 +11,93 @@ public class AddCustomerPage {
 
     public AddCustomerPage(WebDriver driver) {
         this.driver = driver;
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
 
-    @FindBy(xpath="(//i[@class='nav-icon far fa-user']//following::p)[1]")
+    @FindBy(xpath = "(//i[@class='nav-icon far fa-user']//following::p)[1]")
     WebElement linkCustomers;
 
-    @FindBy(xpath="//p[text()=' Customers']")
+    @FindBy(xpath = "//p[text()=' Customers']")
     WebElement sublinkCustomers;
 
-    @FindBy(xpath= "//i[@class='fas fa-plus-square']//parent::a")
+    @FindBy(xpath = "//i[@class='fas fa-plus-square']//parent::a")
     WebElement addNewButton;
 
-    @FindBy(id="Email")
+    @FindBy(id = "Email")
     WebElement emailField;
 
-    @FindBy(id="Password")
+    @FindBy(id = "Password")
     WebElement passwordField;
 
-    @FindBy(id="Firstname")
+    @FindBy(id = "Firstname")
     WebElement firstNameField;
 
-    @FindBy(id="lastName")
+    @FindBy(id = "lastName")
     WebElement lastNameField;
 
-    @FindBy(xpath= "//label[contains(text(),'Male')]//preceding-sibling::input")
+    @FindBy(xpath = "//label[contains(text(),'Male')]//preceding-sibling::input")
     WebElement radioMale;
 
-    @FindBy(id= "DateOfBirth")
+    @FindBy(id = "DateOfBirth")
     WebElement dobField;
 
-    @FindBy(id= "Company")
+    @FindBy(id = "Company")
     WebElement companyField;
 
-    @FindBy(id= "AdminComment")
+    @FindBy(id = "AdminComment")
     WebElement adminCommentField;
 
-    @FindBy(xpath="(//button[@type='submit'])[2]")
+    @FindBy(xpath = "(//button[@type='submit'])[2]")
     WebElement saveButton;
 
+    //Return the title of the page
+    public String getPageTitle() {
+        return driver.getTitle();
+    }
+
+    public void clickCustomerMenuItem() {
+        linkCustomers.click();
+    }
+
+    public void clickCustomerSubMenuItem() {
+        sublinkCustomers.click();
+    }
+
+    public void clickAddNewCustomer() {
+        addNewButton.click();
+    }
+
+    public void setEmailField(String email) {
+        emailField.sendKeys(email);
+    }
+
+    public void setPasswordField(String pass) {
+        passwordField.sendKeys(pass);
+    }
+
+    public void setFirstNameField(String fName) {
+        passwordField.sendKeys(fName);
+    }
+
+    public void setLastNameField(String lName) {
+        passwordField.sendKeys(lName);
+    }
+
+    public void selectGender() {
+        radioMale.click();
+    }
+
+    public void setDobField(String dob) {
+        dobField.sendKeys(dob);
+    }
+
+    public void setCompanyField(String company) {
+        companyField.sendKeys(company);
+    }
+
+    public void clickSave()
+    {
+        saveButton.click();
+    }
 }
